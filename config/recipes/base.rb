d30ee19152d63f67a5f991b557df19dde5ef815d
ivanbenko <bivans209@gmail.com>
2015-02-25 17:58:55 +0100

    run rake through capistrano

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -21,3 +21,16 @@ namespace :deploy do
   task :stop do
   end
 end
+
+desc "Run rake task on server"
+task :sake do
+  on roles(:app), in: :sequence, wait: 5 do
+    within release_path do
+      as :deployer do
+        with rails_env: fetch(:stage) do
+          execute :rake, ENV['task'], "RAILS_ENV=#{fetch(:stage)}"
+        end
+      end
+    end
+  end
+end
